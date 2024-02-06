5b17888fc372b582ff284c7b11cf93bf8cc4adbd
sebastianrosenblunnhellman <sebastianrosenblunnhellman@gmail.com>
2024-02-06 19:48:39 -0300

    funciones con su dataset propio agregadas

--- router/BestDeveloper.py
+++ router/BestDeveloper.py
@@ -1,37 +1,23 @@
 import pandas as pd
 import pyarrow.parquet as pq
 import pyarrow as pa
+import ast
 
 def BestDeveloper(anio: int):
-    # Ruta de los archivos Parquet
-    games_parquet = 'data/steam_games.parquet'
-    reviews_parquet = 'data/user_reviews.parquet'
+    # Cargar los datos desde el archivo Parquet
+    file_path = 'data/Best_Developer_Year.parquet'
+    table = pq.read_table(file_path)
+    tabla_Best_Developer_Year = table.to_pandas()
 
-    # Utiliza PyArrow para leer el archivo Parquet
-    tabla_games = pq.read_table(games_parquet)
-    tabla_reviews = pq.read_table(reviews_parquet)
+    # Filtrar por anio
+    data_anio = tabla_Best_Developer_Year[tabla_Best_Developer_Year['Anio'] == anio]
 
-    # Leer las tablas en DataFrames de Pandas
-    df_steam_games = tabla_games.to_pandas()
-    df_user_reviews = tabla_reviews.to_pandas()
-
-    # Merge de los DataFrames utilizando "item_id" como clave
-    merged_df = pd.merge(df_steam_games, df_user_reviews, on='item_id', how='inner')
-
-    # Filtrar el DataFrame por el anio dado
-    df_filtered = merged_df[merged_df['release_year'] == anio]
-
-    # Filtrar por juegos recomendados y comentarios positivos
-    df_filtered = df_filtered[(df_filtered['recommend'] == True) & (df_filtered['sentiment_analysis'] == 2.0)]
-
-    # Agrupar por desrrollador y contar la cantidad de juegos recomendados
-    developer_counts = df_filtered.groupby('developer')['item_id'].count()
-
-    # Obtener los tres mejores desarrolladores
-    top_developers = developer_counts.nlargest(3)
-    
-
-    # Crear el formato de salida
-    output = [{"Puesto {}: ".format(i+1) + developer: count} for i, (developer, count) in enumerate(top_developers.items())]
-
-    return output
+    # Obtener los desarrolladores para el anio dado
+    if not data_anio.empty:
+        desarrolladores = data_anio['Desarrolladores'].iloc[0]
+        # Convertir la matriz numpy a lista de diccionarios
+        desarrolladores = desarrolladores.tolist()
+        desarrolladores = ast.literal_eval(str(desarrolladores))
+        return [{"Puesto " + str(i+1): list(d.keys())[0]} for i, d in enumerate(desarrolladores)]
+    else:
+        return "No se encontraron datos para el anio especificado."
--- router/DeveloperReviews.py
+++ router/DeveloperReviews.py
@@ -3,33 +3,24 @@ import pyarrow.parquet as pq
 import pyarrow as pa
 
 def DeveloperReviews(desarrolladora: str):
-    # Ruta de los archivos Parquet
-    games_parquet = 'data/steam_games.parquet'
-    reviews_parquet = 'data/user_reviews.parquet'
-
-    # Utiliza PyArrow para leer el archivo Parquet
-    tabla_games = pq.read_table(games_parquet)
-    tabla_reviews = pq.read_table(reviews_parquet)
-
-    # Leer las tablas en DataFrames de Pandas
-    df_steam_games = tabla_games.to_pandas()
-    df_user_reviews = tabla_reviews.to_pandas()
-
-    # Merge de los DataFrames utilizando "item_id" como clave
-    merged_df = pd.merge(df_steam_games, df_user_reviews, on='item_id', how='inner')
-
-    # Filtrar el DataFrame para obtener solo las filas correspondientes a la desarrolladora dada
-    df_dev = merged_df[merged_df['developer'] == desarrolladora]
-
-    # Inicializar el diccionario para almacenar los resultados
-    results = {desarrolladora: {"Negative": 0, "Positive": 0}}
-
-    # Iterar sobre las filas del DataFrame y contar las reseñas positivas y negativas
-    for index, row in df_dev.iterrows():
-        if row['sentiment_analysis'] == 1.0:
-            results[desarrolladora]["Negative"] += 1
-        elif row['sentiment_analysis'] == 2.0:
-            results[desarrolladora]["Positive"] += 1
-
-    return results
-
+    # Cargar los datos desde el archivo Parquet
+    file_path = 'data/Developer_Reviews.parquet'
+    table = pq.read_table(file_path)
+    df = table.to_pandas()
+    
+    # Filtrar el DataFrame para obtener las filas correspondientes a la desarrolladora proporcionada
+    filtered_df = df[df['developer'] == desarrolladora]
+    
+    # Calcular la suma de las reseñas positivas y negativas
+    total_negative = int(filtered_df['Negative'].sum())
+    total_positive = int(filtered_df['Positive'].sum())
+    
+    # Crear el diccionario de retorno
+    result = {
+        desarrolladora: {
+            "Negative": total_negative,
+            "Positive": total_positive
+        }
+    }
+    
+    return result
